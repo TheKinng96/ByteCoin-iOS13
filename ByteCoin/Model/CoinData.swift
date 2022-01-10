a26858c7424659c69f41b95f51d8aa805d55c346
G3N <yap@corekara.co.jp>
2022-01-10 18:06:32 +0900

    done the app, added api call, formatted strings and the view

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Yap Feng Yuan on 2022/01/09.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Codable {
+    let rate: Double
+}
--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -8,12 +8,59 @@
 
 import Foundation
 
+protocol CoinManagerDelegate {
+    func didUpdateCurrency(price: String, currency: String)
+    func updateFailed(error: Error)
+}
+
+extension Double {
+    func withCommas() -> String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        numberFormatter.maximumFractionDigits = 2
+        return numberFormatter.string(from: NSNumber(value:self))!
+    }
+}
+
 struct CoinManager {
-    
+    var delegate: CoinManagerDelegate?
+
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+    let apiKey = "AB37CAAE-44F9-4599-AF46-BD21255B1DD3"
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
-    
+    func getCoinPrice(for currency: String) {
+        let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
+
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, URLResponse, error) in
+                if error != nil {
+                    self.delegate?.updateFailed(error: error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    if let bitcoinPrice = parseJSON(safeData) {
+                        self.delegate?.didUpdateCurrency(price: bitcoinPrice.withCommas(), currency: currency)
+                    }
+                }
+            }
+            
+            task.resume()
+        }
+    }
+}
+
+func parseJSON(_ data: Data) -> Double? {
+    let decoder = JSONDecoder()
+    do {
+        let decodedData = try decoder.decode(CoinData.self, from: data)
+        let currentRate = decodedData.rate;
+        print(currentRate)
+        return currentRate
+    } catch {
+        return nil
+    }
 }
